56796ea675e3515675d7402b43978ddce8136ec5
PC40\userPC35 <jangel94expo00@gmail.com>
2017-01-18 13:35:07 +0100

    feat: implementar metodo delete para dorsal

--- src/main/java/com/tsystems/ecrono/repository/DorsalRepository.java
+++ src/main/java/com/tsystems/ecrono/repository/DorsalRepository.java
@@ -12,9 +12,14 @@ public interface DorsalRepository extends JpaRepository<DorsalEntity, Long> {
 
     DorsalEntity findByRaceIdAndRunner(Long raceId, RunnerEntity runnerEntity);
 
-    // Consult en HQL
+    // Consulta en HQL
     @Query(value = "FROM DorsalEntity  d LEFT JOIN FETCH d.runner WHERE d.raceId =?1")
     List<DorsalEntity> findByRaceId(Long raceId);
 
-    void deleteByRaceIdAndRunner(Long raceId, RunnerEntity runnerEntity);
+    @Query(value = "FROM DorsalEntity d LEFT JOIN d.runner r WHERE d.raceId =?1 AND r.id = ?2")
+    DorsalEntity findByRaceIdAndRunner(Long raceId, Long runnerId);
+
+    @Query(value = "delete from dorsals where race_id =?1 and runner_id =?2", nativeQuery = true)
+    void deleteByRaceIdAndRunner(Long raceId, Long runnerId);
+
 }
